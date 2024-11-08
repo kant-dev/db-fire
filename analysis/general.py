63ae4dab61da1ea511e9e30a0892682688c1fa2d
KauaCantanhedeSantos <kaua.cantanhede.santos@gmail.com>
2024-11-08 19:40:10 -0300

    alteracao-visualizador

--- analysis/general.py
+++ analysis/general.py
@@ -10,58 +10,59 @@ def general_display(df):
       ano = st.sidebar.selectbox("Ano para Análise", anos_disponiveis)
       ano_filtrado = df[df['Ano'] == ano]
       
-      # 4 - Evolução Mensal do Risco de Fogo em Relação à Potência Radiativa de Fogo (FRP)
-      st.header('Hipotese 1')
-      fig_h1 = plotagem_h1(ano_filtrado)
-      if fig_h1:
-            st.plotly_chart(fig_h1)
-      else:
-            st.write("Gráfico não disponível para o ano selecionado.")
-      
-      # 5 -Comparação do Risco de Fogo Mensal por Bioma no Maranhão
-      st.header('Hipotese 2')     
+      # 1 - Evolução Mensal do Risco de Fogo em Relação à Potência Radiativa de Fogo (FRP)
+      st.header(' Qual bioma apresenta maior risco de fogo no Maranhão?')     
       fig_h2 = plotagem_h2(ano_filtrado)
       if fig_h2:
             st.plotly_chart(fig_h2)
       else:
             st.write("Gráfico não disponível para o ano selecionado.")
       
-
-      # 2 -  Influência dos Dias Sem Chuva no Risco de Fogo
-      st.header('Hipotese 3')
+      # 2 
+      st.header('O aumento dos dias sem chuva está associado ao aumento do risco de fogo nos municípios mais afetados?')
+      fig_h7 = plotagem_h7(ano_filtrado)
+      if fig_h7:
+            st.plotly_chart(fig_h7)
+      else: 
+            st.write("Gráfico não disponível para o ano selecionado.")
+      
+      # 3
+      st.header('Como a intensidade das queimadas varia ao longo do ano nos diferentes biomas do Maranhão?')
       fig_h3 = plotagem_h3(ano_filtrado)
       if fig_h3:
             st.plotly_chart(fig_h3)
       else : 
             st.write("Gráfico não disponível para o ano selecionado.")
       
-      # 1 - Sazonalidade do Risco de Fogo
-      st.header('Hipotese 4')
+      # 4
+      st.header('Existe uma correlação entre o risco de fogo e a intensidade das queimadas ao longo do ano?')
+      fig_h1 = plotagem_h1(ano_filtrado)
+      if fig_h1:
+            st.plotly_chart(fig_h1)
+      else:
+            st.write("Gráfico não disponível para o ano selecionado.")
+      
+      # 5
+      st.header('O aumento nos dias sem chuva está diretamente associado ao aumento no risco de fogo?')
       fig_out = plotagem_h4(ano_filtrado)
       if fig_out:
             st.plotly_chart(fig_out)
       else : 
             st.write("Gráfico não disponível para o ano selecionado.")
       
-      # 5 - Relação entre Precipitação e Risco de Fogo
-      st.header('Hipotese 5')
+      # 6
+      st.header('Existe uma correlação negativa entre precipitação e risco de fogo?')
       fig_h5 = plotagem_h5(ano_filtrado)
       if fig_h5:
             st.plotly_chart(fig_h5)
       else: 
             st.write("Gráfico não disponível para o ano selecionado.")
-      
-      st.header('Hipotese 6')
+            
+      # 7
+      st.header('Quais municípios apresentam os maiores valores de FRP e, portanto, as queimadas mais intensas?')
       fig_h6 = plotagem_h6(ano_filtrado)
       if fig_h6:
             st.plotly_chart(fig_h6)
-      else: 
-            st.write("Gráfico não disponível para o ano selecionado.")
-      
-      st.header('Hipotese 7')
-      fig_h7 = plotagem_h7(ano_filtrado)
-      if fig_h7:
-            st.plotly_chart(fig_h7)
       else: 
             st.write("Gráfico não disponível para o ano selecionado.")
       
